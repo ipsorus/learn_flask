f83df4ff5084a43f415389fd16a107aa10e43857
Loshkarev Vadim <ipsorus@inbox.ru>
2019-11-16 12:33:41 +0300

    Добавил регистрацию пользователя

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+from webapp.forms import LoginForm
 from webapp.model import db, News
 from webapp.weather import weather_by_city
 
@@ -8,11 +9,17 @@ def create_app():
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    @app.route("/")
+    @app.route('/')
     def index():
         title = 'Новости Python'
         news_list = News.query.order_by(News.published.desc()).all()
         weather = weather_by_city(app.config['WEATHER_BY_CITY'])
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
 
     return app
