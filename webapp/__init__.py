6cf097b9a6e5be92562dc2ebdbe495c9fac62816
Loshkarev Vadim <ipsorus@inbox.ru>
2019-11-19 17:57:24 +0300

    Добавлена модель пользователя и функционал логина и регистрация

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, flash, render_template,  redirect, url_for
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
 from webapp.forms import LoginForm
-from webapp.model import db, News
+from webapp.model import db, News, User
 from webapp.weather import weather_by_city
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     @app.route('/')
     def index():
@@ -18,8 +27,38 @@ def create_app():
 
     @app.route('/login')
     def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = 'Авторизация'
         login_form = LoginForm()
         return render_template('login.html', page_title=title, form=login_form)
+
+    @app.route('/process_login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы успешно залогинились на сайте')
+                return redirect(url_for('index'))
+        flash('Неверно введен логин или пароль')
+        return redirect(url_for('login')) 
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
+
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return 'Привет админ'
+        else:
+            return 'Ты не админ'
+
+    
 
     return app
