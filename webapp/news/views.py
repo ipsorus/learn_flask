1ca931227f26b32255617f1d19885eab20d22191
Loshkarev Vadim <ipsorus@inbox.ru>
2019-11-19 22:13:35 +0300

    Рефакторинг кода, использование Blueprint

---
+++ webapp/news/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, current_app, render_template
+
+from webapp.news.models import News
+from webapp.weather import weather_by_city
+
+blueprint = Blueprint('news', __name__)
+@blueprint.route('/')
+def index():
+    title = 'Новости Python'
+    news_list = News.query.order_by(News.published.desc()).all()
+    weather = weather_by_city(current_app.config['WEATHER_BY_CITY'])
+    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
